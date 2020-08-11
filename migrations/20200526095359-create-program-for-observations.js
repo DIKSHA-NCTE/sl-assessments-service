3b39df5ba7c34ac4ff8656ba4ff3a23a399282fe
aman-tunerlabs <aman@tunerlabs.com>
2020-08-11 23:34:41 +0530

    migrations updated

--- migrations/20200526095359-create-program-for-observations.js
+++ migrations/20200526095359-create-program-for-observations.js
@@ -62,173 +62,180 @@ module.exports = {
      let updatePrograms = {};
      let newSolutions = {};
 
-    for( 
-      let observationSolution = 0;
-      observationSolution < observationsSolution.length;
-      observationSolution ++
-    ) {
+    if (observationsSolution.length > 0 ) {
 
-      let solutionData = observationsSolution[observationSolution];
-      let programExternalId = 
-      solutionMapToProgram[solutionData._id] && 
-      solutionMapToProgram[solutionData._id].externalId ? 
-      solutionMapToProgram[solutionData._id].externalId : 
-      solutionData.externalId;
-
-      // If solution is having existing program.
-      
-      if(
-        solutionMapToProgram[solutionData._id] && 
-        solutionMapToProgram[solutionData._id].existingProgram
+      for( 
+        let observationSolution = 0;
+        observationSolution < observationsSolution.length;
+        observationSolution ++
       ) {
-
-        let program = 
-        await db.collection("programs").findOneAndUpdate({
-          externalId : programExternalId
-        },{
-          $addToSet : { components : ObjectID(solutionData._id) }
-        },{ returnNewDocument	: true });
+  
+        let solutionData = observationsSolution[observationSolution];
+        let programExternalId = 
+        solutionMapToProgram[solutionData._id] && 
+        solutionMapToProgram[solutionData._id].externalId ? 
+        solutionMapToProgram[solutionData._id].externalId : 
+        solutionData.externalId;
+  
+        // If solution is having existing program.
         
-        if( !updatePrograms[programExternalId] ) {
-          updatePrograms[programExternalId] = {};
-          updatePrograms[programExternalId] = {
-            name : program.value.name,
-            externalId : programExternalId,
-            _id : ObjectID(program.value._id),
-            description : program.value.description
-          };
-        }
-
-      } else {
-
-        if( !createPrograms[programExternalId] ) {
+        if(
+          solutionMapToProgram[solutionData._id] && 
+          solutionMapToProgram[solutionData._id].existingProgram
+        ) {
+  
+          let program = 
+          await db.collection("programs").findOneAndUpdate({
+            externalId : programExternalId
+          },{
+            $addToSet : { components : ObjectID(solutionData._id) }
+          },{ returnNewDocument	: true });
           
-          createPrograms[programExternalId] = {
-            "name" : 
-            solutionMapToProgram[solutionData._id] &&
-            solutionMapToProgram[solutionData._id].name ?  
-            solutionMapToProgram[solutionData._id].name : solutionData.name,
-            
-            externalId : programExternalId,
-            "description" :  
-            solutionMapToProgram[solutionData._id] && 
-            solutionMapToProgram[solutionData._id].description ? 
-            solutionMapToProgram[solutionData._id].description :
-            solutionData.description,
-
-            "owner" : solutionData.author,
-            "createdBy" : solutionData.createdBy,
-            "updatedBy" : solutionData.updatedBy,
-            "isDeleted" : false,
-            "status" : "active",
-            "resourceType" : [ 
-              "program"
-            ],
-            "language" : [ 
-              "English"
-            ],
-            "keywords" : [],
-            "concepts" : [],
-            "createdFor" : [ 
-              "0126427034137395203", 
-              "0124487522476933120"
-            ],
+          if( !updatePrograms[programExternalId] ) {
+            updatePrograms[programExternalId] = {};
+            updatePrograms[programExternalId] = {
+              name : program.value.name,
+              externalId : programExternalId,
+              _id : ObjectID(program.value._id),
+              description : program.value.description
+            };
+          }
   
-            "imageCompression" : {
-              "quality" : 10
-            },
-  
-            "updatedAt" : new Date(),
-            "startDate" : new Date(),
-            "endDate" : new Date(),
-            "createdAt" : new Date(),
-            "isAPrivateProgram" : false,
-            "components" : []
-          };
-        }
-        
-        createPrograms[programExternalId].components.push(
-          ObjectID(solutionData._id)
-        );
-
-      }
-
-      if( !newSolutions[solutionData.externalId] ) {
-        
-        let newSolution = _.cloneDeep(solutionData);
-
-        if(newSolution.externalId.includes("TEMPLATE")) {
-          newSolution.externalId = newSolution.externalId.replace("-OBSERVATION-TEMPLATE","");
-          newSolution.externalId = newSolution.externalId.replace("-TEMPLATE","");
         } else {
-          newSolution.externalId = newSolution.externalId + "-BASE-SOLUTION";
+  
+          if( !createPrograms[programExternalId] ) {
+            
+            createPrograms[programExternalId] = {
+              "name" : 
+              solutionMapToProgram[solutionData._id] &&
+              solutionMapToProgram[solutionData._id].name ?  
+              solutionMapToProgram[solutionData._id].name : solutionData.name,
+              
+              externalId : programExternalId,
+              "description" :  
+              solutionMapToProgram[solutionData._id] && 
+              solutionMapToProgram[solutionData._id].description ? 
+              solutionMapToProgram[solutionData._id].description :
+              solutionData.description,
+  
+              "owner" : solutionData.author,
+              "createdBy" : solutionData.createdBy,
+              "updatedBy" : solutionData.updatedBy,
+              "isDeleted" : false,
+              "status" : "active",
+              "resourceType" : [ 
+                "program"
+              ],
+              "language" : [ 
+                "English"
+              ],
+              "keywords" : [],
+              "concepts" : [],
+              "createdFor" : [ 
+                "0126427034137395203", 
+                "0124487522476933120"
+              ],
+    
+              "imageCompression" : {
+                "quality" : 10
+              },
+    
+              "updatedAt" : new Date(),
+              "startDate" : new Date(),
+              "endDate" : new Date(),
+              "createdAt" : new Date(),
+              "isAPrivateProgram" : false,
+              "components" : []
+            };
+          }
+          
+          createPrograms[programExternalId].components.push(
+            ObjectID(solutionData._id)
+          );
+  
         }
-
-        newSolution.program = programExternalId;
-        newSolution.isAPrivateProgram =  false;
-        newSolutions[solutionData.externalId] = _.omit(newSolution,["_id"]);
+  
+        if( !newSolutions[solutionData.externalId] ) {
+          
+          let newSolution = _.cloneDeep(solutionData);
+  
+          if(newSolution.externalId.includes("TEMPLATE")) {
+            newSolution.externalId = newSolution.externalId.replace("-OBSERVATION-TEMPLATE","");
+            newSolution.externalId = newSolution.externalId.replace("-TEMPLATE","");
+          } else {
+            newSolution.externalId = newSolution.externalId + "-BASE-SOLUTION";
+          }
+  
+          newSolution.program = programExternalId;
+          newSolution.isAPrivateProgram =  false;
+          newSolutions[solutionData.externalId] = _.omit(newSolution,["_id"]);
+        }
       }
+
     }
 
-     let programDocuments = await db.collection("programs").insertMany(
-       Object.values(createPrograms)
-     );
-
-     let programExternalIdToData = programDocuments.ops.reduce(
-      (ac, program) => ({
-          ...ac,
-          [program.externalId]: {
-            name : program.name,
-            externalId : program.externalId,
-            _id : ObjectID(program._id),
-            description : program.description
-          }
-      }), {});
-
-      programExternalIdToData = _.merge(programExternalIdToData,updatePrograms);
-
-      await Promise.all(observationsSolution.map(async solution => {
-
-        let baseSolution = await db.collection("solutions").insertOne(
-          _.omit(newSolutions[solution.externalId],["program"])
-        );
-
-        let updateSolution = {
-          programName : programExternalIdToData[newSolutions[solution.externalId].program].name,
-          programExternalId : programExternalIdToData[newSolutions[solution.externalId].program].externalId,
-          programId : programExternalIdToData[newSolutions[solution.externalId].program]._id,
-          programDescription : programExternalIdToData[newSolutions[solution.externalId].program].description,
-          parentSolutionId :  ObjectID(baseSolution.ops[0]._id),
-          isReusable : false,
-          isAPrivateProgram : false
-        };
-
-        await db.collection("solutions").updateOne({
-          _id : solution._id
-        },{
-          $set : updateSolution
-        });
-
-        let observationUpdate = {
-          programId : updateSolution.programId,
-          programExternalId : updateSolution.programExternalId,
-          isAPrivateProgram : false
-        }
-
-        await db.collection("observations").updateMany({
-          solutionId : solution._id
-        },{
-          $set : observationUpdate
-        });
-
-        await db.collection("observationSubmissions").updateMany({
-          solutionId : solution._id
-        },{
-          $set : observationUpdate
-        });
-
-        return;
-      }));
+    if ( Object.values(createPrograms).length > 0 ) {
+      
+      let programDocuments = await db.collection("programs").insertMany(
+        Object.values(createPrograms)
+      );
+ 
+      let programExternalIdToData = programDocuments.ops.reduce(
+       (ac, program) => ({
+           ...ac,
+           [program.externalId]: {
+             name : program.name,
+             externalId : program.externalId,
+             _id : ObjectID(program._id),
+             description : program.description
+           }
+       }), {});
+ 
+       programExternalIdToData = _.merge(programExternalIdToData,updatePrograms);
+ 
+       await Promise.all(observationsSolution.map(async solution => {
+ 
+         let baseSolution = await db.collection("solutions").insertOne(
+           _.omit(newSolutions[solution.externalId],["program"])
+         );
+ 
+         let updateSolution = {
+           programName : programExternalIdToData[newSolutions[solution.externalId].program].name,
+           programExternalId : programExternalIdToData[newSolutions[solution.externalId].program].externalId,
+           programId : programExternalIdToData[newSolutions[solution.externalId].program]._id,
+           programDescription : programExternalIdToData[newSolutions[solution.externalId].program].description,
+           parentSolutionId :  ObjectID(baseSolution.ops[0]._id),
+           isReusable : false,
+           isAPrivateProgram : false
+         };
+ 
+         await db.collection("solutions").updateOne({
+           _id : solution._id
+         },{
+           $set : updateSolution
+         });
+ 
+         let observationUpdate = {
+           programId : updateSolution.programId,
+           programExternalId : updateSolution.programExternalId,
+           isAPrivateProgram : false
+         }
+ 
+         await db.collection("observations").updateMany({
+           solutionId : solution._id
+         },{
+           $set : observationUpdate
+         });
+ 
+         await db.collection("observationSubmissions").updateMany({
+           solutionId : solution._id
+         },{
+           $set : observationUpdate
+         });
+ 
+         return;
+       }));
+     }
 
       return;
 
